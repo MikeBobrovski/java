3f16f54a5817d6d4877395b68580a46351996626
MikeBobrovski <61408911+MikeBobrovski@users.noreply.github.com>
2020-08-03 16:05:19 +0300

    Add files via upload

---
+++ hw24/src/main/java/MyServlet.java
@@ -0,0 +1,20 @@
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+//@WebServlet("/hello")
+public class MyServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try (ServletOutputStream os = resp.getOutputStream()) {
+            os.print("Hello");
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
